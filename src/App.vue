77b1f2d17e746e9f5ff452233fe2285beb60b78b
simsoll <simon@soellingvraa.dk>
2017-03-05 17:47:57 +0100

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Parent Vue instance: {{ total }}</p>
+    <button-counter v-bind:start="0" v-on:increment="incrementTotal"></button-counter>
+    <button-counter v-bind:start="1" v-on:increment="incrementTotal"></button-counter>
+  </div>
+</template>
+
+<script>
+import ButtonCounter from './ButtonCounter.vue'
+
+export default {
+  components: {
+    buttonCounter: ButtonCounter
+  },
+  data: function() {
+    return {
+      total: 0
+    }
+  },
+  methods: {
+    incrementTotal: function() {
+      this.total += 1
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/ButtonCounter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <button v-on:click="increment">
+            {{ totalCount }}
+        </button>
+    </div>        
+</template>
+
+<script>
+export default {
+    props: {
+        start: {
+            type: Number,
+            required: true
+        }  
+    },
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter += 1
+            this.$emit('increment')
+        }
+    },
+    computed: {
+        totalCount: function() {
+            return this.start + this.counter;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
